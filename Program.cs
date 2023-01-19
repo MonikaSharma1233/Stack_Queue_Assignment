a801e5638409bc733fd72a527b5d37c3f8e06bc8
firsttoken <monikasharma12330@gmail.com>
2023-01-19 23:24:10 +0530

    Main method

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Stack_and_Queue_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            stack.Pop();
+            stack.IsEmpty();
+            //stack.Display();
+            //--------------------Queue---------------------
+            Queue_Assignment qa = new Queue_Assignment();
+            qa.Enqueue(56);
+            qa.Enqueue(30);
+            qa.Enqueue(70);
+            qa.Display();
+            qa.Dequeue();
+            qa.Display();
+            int size = qa.Size();
+            Console.WriteLine("Size of Queue is: "+size);
+        }
+    }
+}
